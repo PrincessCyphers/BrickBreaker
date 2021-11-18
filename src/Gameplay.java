1cb35064682d0f9f3263f41d6b296f77b186adc0
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-18 21:50:33 +0000

    created game elements; ball, paddle, and panel with border and bg.

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -10,12 +11,41 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private int score = 0;
     private int totalBricks = 30;
     private Timer timer;
-    private int delay = 8;
+    private int delay = 8;          //milliseconds
     private int playerX = 450;
     private int ballPosX = 200;
     private int ballPosY = 350;
     private int ballXdir = -1;
     private int ballYdir = 2;
+
+    public Gameplay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics g) {
+        // bg
+        g.setColor(Color.BLACK);
+        g.fillRect(1,1, 890, 690);
+
+        // border
+        g.setColor(Color.white);
+        g.fillRect(0, 0, 3, 690);
+        g.fillRect(0, 0, 890, 3);
+        g.fillRect(895, 0, 3, 690 );
+
+        //paddle
+        g.setColor(Color.lightGray);
+        g.fillRect(playerX, 550, 150, 10);
+
+
+        //ball
+        g.setColor(Color.MAGENTA);
+        g.fillOval(ballPosX, ballPosY, 20, 20);
+    }
 
 
 
