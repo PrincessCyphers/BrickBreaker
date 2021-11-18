81cd1f80862e38c8be9b9a566c64fbebd0d9d505
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-18 19:40:37 +0000

    created game frame and set parameters.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Gameplay gamePlay = Gameplay();
+        frame.setTitle("BRICK BREAKER");
+        frame.setBounds(10, 10, 900, 700);
+        frame.setLocationRelativeTo(null);                  // so frame is central to page on opening
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.add(gamePlay);
 
     }
 }
