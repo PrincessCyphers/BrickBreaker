4e744de7ee4cb5b28e09b95f94a8e65c27c25c7f
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-20 18:19:28 +0000

    reordered so setVisible comes at end. Suspect that maybe why game wasnt functioning with previous commit

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         Gameplay gamePlay = new Gameplay();
-        frame.setTitle("BRICK BREAKER");
         frame.setBounds(10, 10, 900, 700);
+        frame.setTitle("BRICK BREAKER");
         frame.setLocationRelativeTo(null);                  // so frame is central to page on opening
-        frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.add(gamePlay);
+        frame.setVisible(true);
 
     }
 }
